7ef792d17b132165e4eb07845d5e17d4b28c0d0d
Tunahan Karaman <tunahankaraman@outlook.com.tr>
2022-09-18 15:56:49 +0300

    arrays demo and loops

---
+++ 1st-week_1st-task/Loops/DoWhileLoop/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] args) 
+	{
+		int sayi = 20;
+		do {
+			System.out.println(sayi);
+			sayi--;
+		} while (sayi >= 10);
+	}
+}
---
+++ 1st-week_1st-task/Loops/While/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) 
+	{
+		int sayi = 2;
+		while (sayi <= 10) 
+		{
+			System.out.println(sayi);
+			sayi++;
+		}
+		System.out.println("Döngü bitti : ");
+	}
+}
